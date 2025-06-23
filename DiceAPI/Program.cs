dd233f2f5d9277bba45ac38d93a21ff89d769f06
PawelFlorczak <pawelfsanok@gmail.com>
2025-06-24 00:51:14 +0200

    reverted the fucin database url

--- DiceAPI/Program.cs
+++ DiceAPI/Program.cs
@@ -1,37 +1,14 @@
-using Microsoft.EntityFrameworkCore;
 using DiceAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Pobierz connection string z appsettings.json lub zmiennej środowiskowej DATABASE_URL
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-                       ?? Environment.GetEnvironmentVariable("DATABASE_URL");
-
-// UWAGA: jeśli connection string jest w formacie postgres://... (Render tak daje),
-// musimy przekonwertować go na format zrozumiały dla Npgsql/EF Core
-if (connectionString.StartsWith("postgres://"))
-{
-    var uri = new Uri(connectionString);
-    var userInfo = uri.UserInfo.Split(':');
-
-    connectionString =
-        $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};" +
-        $"Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
-    
-    Console.WriteLine("ENV DATABASE_URL: " + Environment.GetEnvironmentVariable("DATABASE_URL"));
-    if (string.IsNullOrEmpty(connectionString))
-    {
-        throw new InvalidOperationException("Brak ustawionej zmiennej DATABASE_URL ani wpisu DefaultConnection w appsettings.");
-    }
-}
-
 builder.Services.AddDbContext<DiceContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
