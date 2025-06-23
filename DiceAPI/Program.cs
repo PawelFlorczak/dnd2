ab0f1b92f7cfcbb117b03d1b9abe9a3d6a75d2f9
PawelFlorczak <pawelfsanok@gmail.com>
2025-06-23 23:29:34 +0200

    Internal Database URL

--- DiceAPI/Program.cs
+++ DiceAPI/Program.cs
@@ -1,14 +1,31 @@
-using DiceAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using DiceAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Pobierz connection string z appsettings.json lub zmiennej środowiskowej DATABASE_URL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? Environment.GetEnvironmentVariable("DATABASE_URL");
+
+// UWAGA: jeśli connection string jest w formacie postgres://... (Render tak daje),
+// musimy przekonwertować go na format zrozumiały dla Npgsql/EF Core
+if (connectionString.StartsWith("postgres://"))
+{
+    var uri = new Uri(connectionString);
+    var userInfo = uri.UserInfo.Split(':');
+
+    connectionString =
+        $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+
 builder.Services.AddDbContext<DiceContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
-app.UseHttpsRedirection();
+
 app.MapControllers();
+
 app.Run();
