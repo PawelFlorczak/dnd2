0967b154bb4540f2dfba6b48ae2599aed2bd5ed1
Paweł <pawelfsanok@gmail.com>
2025-09-29 11:12:48 +0200

    net8 & nullable PlayerName

--- DiceAPI/Models/DiceRoll.cs
+++ DiceAPI/Models/DiceRoll.cs
@@ -1,9 +1,10 @@
+#nullable enable
 namespace DiceAPI.Models;
 
 public class DiceRoll
 {
     public int Id { get; set; }
-    public string PlayerName { get; set; }
+    public string? PlayerName { get; set; }
     public int Sides { get; set; }
     public int Result { get; set; }
     public DateTime Timestamp { get; set; }
--- DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
+++ DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0f1b92f7cfcbb117b03d1b9abe9a3d6a75d2f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd233f2f5d9277bba45ac38d93a21ff89d769f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
