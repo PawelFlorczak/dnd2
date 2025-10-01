797a6aa197789a6b42eadc14c9c54188389d9f66
Paweł <pawelfsanok@gmail.com>
2025-10-01 14:44:00 +0200

    Websocket test 4 ughhh

--- DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
+++ DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8cbec8a85f105c5b988255dd9b3c918bbdee595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc0b824e0c0071baf50e43c0e52ab2e1342e9dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
