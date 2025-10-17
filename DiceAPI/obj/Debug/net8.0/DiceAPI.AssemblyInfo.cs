aaf94c926aa731b93699268310de8fdd346f10b9
Paweł <pawelfsanok@gmail.com>
2025-10-17 15:59:18 +0200

    motyw poprawa

--- DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
+++ DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3645eba5eba7223370d1e7e8b0a8befde5b9dbd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2105ddcd5e3e7652dece8dc4a0fe152db41888")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
