fa54dcb60b04686c7102600dfa10acbb99363d27
Paweł <pawelfsanok@gmail.com>
2025-10-21 15:52:32 +0200

    UI cosmetics fix(Logic not implemented YET)

--- DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
+++ DiceAPI/obj/Debug/net8.0/DiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf94c926aa731b93699268310de8fdd346f10b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3af8b9e6721a6474e12a4f73dbf28449fc5d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
